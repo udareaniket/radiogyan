d8be6bb297abb8abe6cbd775e1f1cfa7d34a2528
Aniket Udare <udareaniket@gmail.com>
2019-07-30 15:25:20 -0400

    Update radioVolume.js

--- radioVolume.js
+++ radioVolume.js
@@ -8,31 +8,44 @@ jQuery(document)
 						volume();
 					});
 					$('.radio').bind("click", function() {
-						volume();
+						radioSelect();
 					});
 					$('.copy').bind("click", function() {
 						copy();
 					});
 					$('[data-toggle="tooltip"]').tooltip();
+					var multiplier = 0.5;
+					document.getElementById('genericId').click();
+					function radioSelect() {
+						if (document.getElementById('genericId').checked) {
+							document.getElementById('selectedType').innerHTML = 'Generic Ellipsoid Volume';
+							multiplier = 0.523;
+						} else if (document.getElementById('urinaryId').checked) {
+							document.getElementById('selectedType').innerHTML = 'Urinary Bladder Volume';
+							multiplier = 0.625;
+						} else if (document.getElementById('testicularId').checked) {
+							document.getElementById('selectedType').innerHTML = 'Testicular Volume';
+							multiplier = 0.71;
+						} else if (document.getElementById('renalId').checked) {
+							document.getElementById('selectedType').innerHTML = 'Renal Volume';
+							multiplier = 0.523;
+						} else if (document.getElementById('prostateId').checked) {
+							document.getElementById('selectedType').innerHTML = 'Prostate Volume';
+							multiplier = 0.523;
+						}
+						volume();
+					}
 					function volume() {
 						var length = Number($('#length').val());
 						var breadth = Number($('#breadth').val())
 						var width = Number($('#width').val());
-						var multiplier = 0.5;
-						if (!(length == 0 || breadth == 0 || width == 0)) {
-							if (document.getElementById('genericId').checked
-									|| document.getElementById('renalId').checked) {
-								multiplier = 0.523;
-							} else if (document.getElementById('urinaryId').checked) {
-								multiplier = 0.625;
-							} else if (document.getElementById('testicularId').checked) {
-								multiplier = 0.71;
-							}
+						console.log($('#length')[0].value)
+						if (!($('#length')[0].value == ''
+								|| $('#breadth')[0].value == '' || $('#width')[0].value == '')) {
 							var result = ((length * breadth * width * multiplier))
 									.toFixed(3);
 							$('#totalVolume').html(result);
 							document.getElementById('totalVolume').value = result;
-							//copy();
 							$('#copyButton').prop("disabled", false);
 						}
 
